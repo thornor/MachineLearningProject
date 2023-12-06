7beae6008d51f054a8f2b5e1018133bcdcfa4d95
unknown <thornor53@gmail.com>
2023-12-06 15:24:46 +0100

    adding test function

--- RNN.py
+++ RNN.py
@@ -5,6 +5,7 @@ import nltk
 import numpy as np
 import torch
 from torch import nn
+import matplotlib.pyplot as plt
 
 
 def data_process():
@@ -45,8 +46,6 @@ def data_process():
         word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
         print("The least picked card is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
 
-        print("\nExample sentence: '%s'" % sentences[0])
-        print("\nExample sentence after Pre-processing: '%s'" % tokenize_sentences[0])
         # Create the training data
         x_train = []
         y_train = []
@@ -56,26 +55,24 @@ def data_process():
                 words.append(word_to_index[w])
             x_train.append(words[:-1])
             y_train.append(words[1:])
-            if len(words) != 44:
-                print(len(words), sent, tokenize_sentences.index(sent))
         x_train=np.array(x_train)
         y_train=np.array(y_train)
     return x_train, y_train, maxlen, len(word_freq), len(sentences)
 
 x_train, y_train, maxlen, dict_size, batch_size = data_process()
 seq_len = maxlen - 1
-x_train = np.array(x_train)
-y_train = np.array(y_train)
 
 def one_hot_encode(sequence, dict_size, seq_len, batch_size):
     # Creating a multi-dimensional array of zeros with the desired output shape
     features = np.zeros((batch_size, seq_len, dict_size), dtype=np.float32)
     
-    # Replacing the 0 at the relevant character index with a 1 to represent that character
+    # Replacing the 0 at the relevant character index with a 1 to represent that card
     for i in range(batch_size):
         for u in range(seq_len):
             features[i, u, sequence[i][u]] = 1
     return features
+
+x_train = one_hot_encode(x_train, dict_size, seq_len, batch_size)
 
 x_train = torch.from_numpy(x_train)
 y_train = torch.Tensor(y_train)
@@ -112,72 +109,52 @@ class Model(nn.Module):
     
     def init_hidden(self, batch_size):
         # This method generates the first hidden state of zeros which we'll use in the forward pass
-        # We'll send the tensor holding the hidden state to the device we specified earlier as well
         hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         return hidden
 
-# torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
-is_cuda = torch.cuda.is_available()
+def test(model):
+    model.train(False)
+    totloss, nbatch = 0., 0
+    out, hidden = model(x_train)
+    loss = criterion(out,y_train.view(-1).long())
+    totloss += loss.item()
+    nbatch += 1
+    totloss /= float(nbatch)
+    model.train(True)
+    return totloss
 
-# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
-if is_cuda:
-    device = torch.device("cuda")
-    print("GPU is available")
-else:
-    device = torch.device("cpu")
-    print("GPU not available, CPU used")
 # Instantiate the model with hyperparameters
-model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=12, n_layers=1)
-# We'll also set the model to the device that we defined earlier (default is CPU)
-model.to(device)
+model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=5, n_layers=42)
 
 # Define hyperparameters
-n_epochs = 100
-lr=0.01
+n_epochs = 500
+lr=0.001
 
 # Define Loss, Optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+totloss_list=[]
+testloss_list=[]
+instances=[i for i in range(n_epochs)]
+
+
 # Training Run
 for epoch in range(1, n_epochs + 1):
+    testloss=test(model)
     optimizer.zero_grad() # Clears existing gradients from previous epoch
-    x_train.to(device)
     output, hidden = model(x_train)
     loss = criterion(output, y_train.view(-1).long())
     loss.backward() # Does backpropagation and calculates gradients
     optimizer.step() # Updates the weights accordingly
-    
+    testloss_list.append(testloss)
+    totloss_list.append(loss.item())
+
     if epoch%10 == 0:
         print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
-        print("Loss: {:.4f}".format(loss.item()))
+        print("testloss: {:.4f}".format(testloss))
+        print("loss: {:.4f}".format(loss.item()))
 
-# This function takes in the model and character as arguments and returns the next character prediction and hidden state
-def predict(model, character):
-    # One-hot encoding our input to fit into the model
-    character = np.array([[char2int[c] for c in character]])
-    character = one_hot_encode(character, dict_size, character.shape[1], 1)
-    character = torch.from_numpy(character)
-    character.to(device)
-    
-    out, hidden = model(character)
-
-    prob = nn.functional.softmax(out[-1], dim=0).data
-    # Taking the class with the highest probability score from the output
-    char_ind = torch.max(prob, dim=0)[1].item()
-
-    return int2char[char_ind], hidden
-
-# This function takes the desired output length and input characters as arguments, returning the produced sentence
-def sample(model, out_len, start='hey'):
-    model.eval() # eval mode
-    start = start.lower()
-    # First off, run through the starting characters
-    chars = [ch for ch in start]
-    size = out_len - len(chars)
-    # Now pass in the previous characters and get a new one
-    for ii in range(size):
-        char, h = predict(model, chars)
-        chars.append(char)
-
-    return ''.join(chars)
+plt.plot(instances, testloss_list)
+plt.plot(instances, totloss_list)
+plt.show()
